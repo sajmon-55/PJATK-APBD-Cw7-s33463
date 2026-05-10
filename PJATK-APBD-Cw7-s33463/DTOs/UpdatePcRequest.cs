d669bdd7bbfa62f7e9cabdab4a6e99412912971c
sajmon-55 <szgodlewski.pl@gmail.com>
2026-05-10 14:34:24 +0200

    Create UpdatePcRequest.cs

---
+++ PJATK-APBD-Cw7-s33463/DTOs/UpdatePcRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PJATK_APBD_Cw7_s33463.DTOs;
+
+public record UpdatePcRequest(
+    [MaxLength(50)]
+    string Name,
+    float Weight,
+    int Warranty,
+    DateTime Date,
+    int Stock
+);
