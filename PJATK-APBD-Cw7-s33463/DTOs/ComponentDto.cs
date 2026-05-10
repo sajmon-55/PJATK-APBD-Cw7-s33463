5895b3ce0c0d3e6729917b255c829781f37b713f
sajmon-55 <szgodlewski.pl@gmail.com>
2026-05-10 14:34:07 +0200

    Create ComponentDto.cs

---
+++ PJATK-APBD-Cw7-s33463/DTOs/ComponentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PJATK_APBD_Cw7_s33463.DTOs;
+
+public record ComponentDto(
+    string Code,
+    string Name,
+    string Description,
+    ManufacturerDto Manufacturer,
+    TypeDto Type
+);
