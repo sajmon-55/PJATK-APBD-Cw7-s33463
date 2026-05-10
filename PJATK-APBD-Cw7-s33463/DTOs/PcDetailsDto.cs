aa083f2a58c48b8fde719027698c9a73c9e141cb
sajmon-55 <szgodlewski.pl@gmail.com>
2026-05-10 14:34:16 +0200

    Create PcDetailsDto.cs

---
+++ PJATK-APBD-Cw7-s33463/DTOs/PcDetailsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PJATK_APBD_Cw7_s33463.DTOs;
+
+public record PcDetailsDto(
+    int Id,
+    string Name,
+    float Weight,
+    int Warranty,
+    DateTime Date,
+    int Stock,
+    IEnumerable<ComponentDto> Components
+);
